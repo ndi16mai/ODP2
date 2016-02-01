4304cd02e9a91d55804b481b6f93874ab1d373ea
wareus <rien90@hotmail.com>
2016-02-01 16:09:53 +0100

    Basic GUI added

--- MP2/src/se/hig/odp2/mp2/gui/Controller.java
+++ MP2/src/se/hig/odp2/mp2/gui/Controller.java
@@ -1,4 +1,38 @@
 package se.hig.odp2.mp2.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    protected void createCourseInstance()
+    {
+        System.out.println("Controller.createCourseInstance");
+    }
+
+    @FXML
+    protected void editCourseInstance()
+    {
+        System.out.println("Controller.editCourseInstance");
+    }
+
+    @FXML
+    protected void registerStudent()
+    {
+        System.out.println("Controller.registerStudent");
+    }
+
+    @FXML
+    protected void unregisterStudent()
+    {
+        System.out.println("Controller.unregisterStudent");
+    }
+
+    @FXML
+    protected void getCourses()
+    {
+        System.out.println("Controller.getCourses");
+    }
+
 }
