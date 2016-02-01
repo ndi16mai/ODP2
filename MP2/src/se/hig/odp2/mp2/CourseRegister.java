10905cd9808ad0e7a37960acf57733830310b5f2
wareus <rien90@hotmail.com>
2016-02-01 15:31:45 +0100

    Author removed

--- MP2/src/se/hig/odp2/mp2/CourseRegister.java
+++ MP2/src/se/hig/odp2/mp2/CourseRegister.java
@@ -2,9 +2,6 @@ package se.hig.odp2.mp2;
 
 import java.util.List;
 
-/**
- * Created by Wareus on 2016-02-01.
- */
 public class CourseRegister
 {
     private List<Course> courses;
--- MP2/src/se/hig/odp2/mp2/Period.java
+++ MP2/src/se/hig/odp2/mp2/Period.java
@@ -1,8 +1,5 @@
 package se.hig.odp2.mp2;
 
-/**
- * Created by Wareus on 2016-02-01.
- */
 public class Period
 {
     private int year, period;
