67dd1ade6f21de00d4369f4a67656288eb7dc9ab
wareus <rien90@hotmail.com>
2016-02-03 07:52:01 +0100

    deafaule value on choisebox

--- MP2/src/se/hig/odp2/mp2/gui/controllers/AddCourseController.java
+++ MP2/src/se/hig/odp2/mp2/gui/controllers/AddCourseController.java
@@ -2,25 +2,36 @@ package se.hig.odp2.mp2.gui.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class AddCourseController
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCourseController implements Initializable
 {
     @FXML
     private TextField name, code;
     @FXML
     private ChoiceBox<Float> points;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        points.getSelectionModel().selectFirst();
+    }
+
     @FXML
     protected void addCourse(ActionEvent event)
     {
+        
+
         Node node = (Node) event.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
         stage.close();
     }
-
 }
