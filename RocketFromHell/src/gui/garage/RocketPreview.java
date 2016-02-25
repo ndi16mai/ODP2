a902015e3c59be0652acd49ed43c3e311f6e557e
wareus <rien90@hotmail.com>
2016-02-25 16:45:21 +0100

    first rocket implemented

--- RocketFromHell/src/gui/garage/RocketPreview.java
+++ RocketFromHell/src/gui/garage/RocketPreview.java
@@ -1,16 +1,10 @@
 package gui.garage;
 
 import gui.Assets;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class RocketPreview extends Pane
 {
